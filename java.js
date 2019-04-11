82db0ab87f3eb636c270ac3c00c272b11d8927a0
mariaelena <girlygamer8989@gmail.com>
2019-04-11 14:23:08 -0700

    username

--- java.js
+++ java.js
@@ -1,16 +1,16 @@
 let existingUserName = localStorage.getItem ('username');
 if (existingUserName){
-    window.alert('now entering whiterose hub ');
-    window.alert('welcome back ' + existingUserName + '😊')
+    window.alert('Now entering, Whiterose Hub ');
+    window.alert('Welcome Back ' + existingUserName + '😊')
 
 
 } else {
-    window.alert('Welcome Newuser to my website');
+    window.alert('Welcome Newuser to my website hope you enjoy your stay here at the whiterose hub');
     let userName = window.prompt('What do you want me to call you');
     if (userName){
         localStorage.setItem('username', userName);
     }
-    window.alert('welcome to my website,' + userName + ' :)');
+    window.alert('welcome to my website,' + userName + ' 😊');
 }
 
 
